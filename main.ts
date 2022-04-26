cb74aa2bd7554100232d5dc3c7572f526d8df1ae
AmandaOvia <101339756+AmandaOvia@users.noreply.github.com>
2022-04-26 09:45:30 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,14 +4,8 @@ input.onButtonPressed(Button.A, function () {
     }
 })
 input.onButtonPressed(Button.AB, function () {
-    for (let x = 0; x <= 9; x++) {
-        basic.showNumber(3)
-        basic.showNumber(5)
-        basic.showNumber(7)
-        basic.showNumber(9)
-        basic.showNumber(11)
-        basic.showNumber(13)
-        basic.showNumber(15)
+    for (let x = 0; x <= 4; x++) {
+        basic.showNumber(2 / x)
     }
 })
 input.onButtonPressed(Button.B, function () {
